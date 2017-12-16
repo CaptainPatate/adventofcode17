209d3a5b08b1810ad4675db781ea213f60d107bf
Amaury Gauthier <me@amaurygauthier.eu>
2017-12-16 15:11:42 +0100

    Day 5 part 1: simplify data structure
    Just store index and a Data.Sequence.
    Run in ~0.2s

--- day5/solution.hs
+++ day5/solution.hs
@@ -1,27 +1,24 @@
 import Debug.Trace
 import qualified Data.Sequence as S
 
-data Zipper a = Zipper (S.Seq a) a (S.Seq a) deriving Show
+data Zipper a = Zipper Int (S.Seq a) deriving Show
 
 fromList :: [a] -> Zipper a
-fromList (x:xs) = Zipper S.empty x (S.fromList xs)
+fromList xs = Zipper 0 (S.fromList xs)
 
 right :: Int -> Maybe (Zipper a) -> Maybe (Zipper a)
 right _ Nothing = Nothing
-right step same@(Just (Zipper l a r))
+right step same@(Just (Zipper i xs))
   | step == 0 = same
-  | S.null r = Nothing
-  | S.length r < step = Nothing
-  | otherwise = Just $ Zipper ((l S.|> a) S.>< (S.take (step - 1) r)) (r `S.index` (step - 1)) (S.drop step r)
+  | (i + step) >= S.length xs = Nothing
+  | otherwise = Just $ Zipper (i + step) xs
 
 left :: Int -> Maybe (Zipper a) -> Maybe (Zipper a)
 left _ Nothing = Nothing
-left step same@(Just (Zipper l a r))
+left step same@(Just (Zipper i xs))
   | step == 0 = same
-  | S.null l = Nothing
-  | len < step = Nothing
-  | otherwise = Just $ Zipper (S.take (len - step) l) (l `S.index` (len - step)) ((S.drop (len - step + 1) l) S.>< (a S.<| r))
-  where len = S.length l
+  | (i - step) < 0 = Nothing
+  | otherwise = Just $ Zipper (i - step) xs
 
 jump :: Int -> Maybe (Zipper a) -> Maybe (Zipper a)
 jump num Nothing = Nothing
@@ -31,7 +28,7 @@ jump num z@(Just _)
 
 step :: Maybe (Zipper Int) -> Maybe (Zipper Int)
 step Nothing = Nothing
-step (Just (Zipper l a r)) = jump a (Just $ Zipper l (a+1) r)
+step (Just (Zipper i xs)) = jump (xs `S.index` i) (Just $ Zipper i (S.adjust (+1) i xs))
 
 readInt :: String -> Int
 readInt = read
@@ -40,7 +37,7 @@ runZipper :: Zipper Int -> Int
 runZipper z = go (Just z) 0
   where
     go (Nothing) count = count
-    go z@(Just (Zipper l a r)) count = if count `mod` 10000 == 0
+    go z@(Just (Zipper _ _)) count = if count `mod` 10000 == 0
                                        then trace (show z) $ go (step z) (count+1)
                                        else go (step z) (count+1)
 
